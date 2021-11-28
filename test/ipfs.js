3e995ac12e3cc2e7ffb6d31f37b026ae298b8ff3
muhammad3132002 <business.investor.success@gmail.com>
2021-11-28 15:01:00 +1000

    updated_version

--- test/ipfs.js
+++ test/ipfs.js
@@ -6,12 +6,18 @@ const entryPointHttp = 'http://127.0.0.1:8080/ipfs/' // process.env.IPFS
 const entryPoint = '/ip4/127.0.0.1/tcp/5001' // process.env.IPFS
 /* const ipfs = require("nano-ipfs-store").at("http://127.0.0.1:8080"); */
 
+// const { useSelector } = require('react-redux') 
+
+
 describe("test for ipfs", function () {
 	it("put data", async function () {
 		try {
+			// const G = useSelector(state => state) 
 			/* const client = create({ host: '1.1.1.1', port: '80', apiPath: '/ipfs/api/v0' } */
 			const client = await create(entryPoint)
-			const data = {
+			
+			const data = 
+			{
 				title: 'Kovan Testnet Ether 1500~3000 for online free crypto game', 
 				contents: `Hello dear
 				I’m blockchain and smart contract developer.
@@ -20,6 +26,7 @@ describe("test for ipfs", function () {
 				Now, Dapp and lots of crypto gambling games exist, but it is not free so all people can’t enjoy it.
 				so our team will make the online free game that deposit kovan and ropstern ETH.`
 			}
+			console.log(data)
 			const contents = JSON.stringify(data)
 			const cid = await client.add(contents)
 			console.log(cid)
